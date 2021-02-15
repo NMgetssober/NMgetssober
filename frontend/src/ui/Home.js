e19b4f6b3289be98b28cd57887b3dadabce165d5
Brandon Whyte <bwhyte@cnm.edu>
2021-02-15 10:56:29 -0700

    updating project

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,9 +1,15 @@
 import React from "react"
+import {Button, Container} from "react-bootstrap";
 
 export const Home = () => {
     return(
         <>
-            <h1>Home</h1>
+            <div>
+                <Container>
+                    <Button>Treatment Centers</Button>
+                    <Button>Community Activities</Button>
+                </Container>
+            </div>
         </>
     )
 }
