16a302a524a76b3fc1c4010967ddc74b38881965
Brandon Whyte <bwhyte@cnm.edu>
2021-02-19 10:22:56 -0700

    updating changes

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,19 +1,22 @@
 import React from "react"
 import './Home.css'
 import logo from './nmgslogo.png'
-import {Container, Image, Row, Button} from "react-bootstrap";
+import {Container, Image, Row, Button, Col} from "react-bootstrap";
 export const Home = () => {
     return(
         <>
             <Container fluid className="mask justify-content-center">
-                <Row className="justify-content-center">
+                <Row className="justify-content-center mt-5">
                     <Image id={"logo"} src={logo}
                            alt="nmgslogo"/>
                 </Row>
             </Container>
             <Container fluid>
-                <Row className="justify-content-center">
-                    <Button id="largeButton" size="lg" variant="outline-light" className="m-3 text-danger btn btn-warning font-weight-bold">Treatment Centers</Button>
+                <Row id="words" className="justify-content-center mt-2 mb-3">Sobriety | Community | Recovery </Row>
+            </Container>
+            <Container fluid>
+                <Row className="justify-content-center mb-5">
+                    <Button id="largeButton" size="lg" variant="outline-light" className="m-3 text-danger font-weight-bold">Treatment Centers</Button>
                     <Button id="largeButton" size="lg" variant="outline-light" className="m-3 text-danger btn btn-warning font-weight-bold">Community Activities</Button>
                 </Row>
             </Container>
