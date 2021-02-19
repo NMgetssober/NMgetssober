6d6397d29930ba2f11771e357d97ee46fa36d55f
Cindy Chen <cchen4@cnm.edu>
2021-02-19 09:48:15 -0700

    updated navbarfooter

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,13 +3,13 @@ import { Route, Switch } from 'react-router'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
-import { MainNav } from './NavBar'
+import { NavBar } from './NavBar'
 import { Footer } from './Footer'
 
 export const App = () => (
     <>
         <BrowserRouter>
-            <MainNav/>
+            <NavBar/>
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/navbar' component={NavBar}/>
