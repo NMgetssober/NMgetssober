63291aa2ebac7746c1de0b86e597241b79e4c9f0
Cindy Chen <cchen4@cnm.edu>
2021-02-22 11:37:15 -0700

    updated landing page, about us, express spinup

---
+++ sql/nmgetssober.sql
@@ -0,0 +1,8 @@
+MYSQL_RANDOM_ROOT_PASSWORD=yes
+MYSQL_DATABASE=nmgetssober
+MYSQL_USER=cchen4
+MYSQL_PASSWORD=qwerty123
+MYSQL_HOST=sql
+sessionSecret=...put.your.secret.here...
+MAILGUN_DOMAIN=sandbox.....Put.your.mailgun.domain.here........mailgun.org
+MAILGUN_API_KEY=...Put.your.mailgun.key.here...
