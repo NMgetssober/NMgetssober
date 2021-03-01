5e041f37cd20ef6a8de44fb9b2ef401a89229447
Cindy Chen <cchen4@cnm.edu>
2021-03-01 16:52:08 -0700

    fixed bug for activation

--- backend/src/utils/profile/updateProfile.ts
+++ backend/src/utils/profile/updateProfile.ts
@@ -3,10 +3,13 @@ import {connect} from "../database.utils";
 
 export async function updateProfile(profile: Profile) {
     try {
+
         const mysqlConnection = await connect();
-        const query : string = 'UPDATE profile SET profileAuthenticationKey = :profileAuthenticationKey, profileUsername = :profileUsername, profileEmail = :profileEmail, profilePassword = :profilePassword WHERE profileId = UUID_TO_BIN(:profileID)';
+
+        const query : string = 'UPDATE profile SET profileAuthenticationKey = :profileAuthenticationKey, profileUsername = :profileUsername, profileEmail = :profileEmail, profilePassword = :profilePassword WHERE profileId = UUID_TO_BIN(:profileId)';
 
         const [rows] = await mysqlConnection.execute(query, profile);
+
         return 'Profile successfully updated'
     } catch (e) {
         console.error(e)
