89100630947aec21fe4653b37ac3bf7b69b4fc06
vchavez7 <vchavez7@cnm.edu>
2021-03-08 14:48:52 -0700

    updated treatment center utils

--- backend/src/utils/treatmentcenter/selectTreatmentCenterByProfileId.ts
+++ backend/src/utils/treatmentcenter/selectTreatmentCenterByProfileId.ts
@@ -6,6 +6,8 @@ export async function selectTreatmentCenterByProfileId(profileId: string) {
 
         const [rows] = await mysqlConnection.execute('SELECT treatmentCenter.treatmentCenterId, treatmentCenter.treatmentCenterName, treatmentCenter.treatmentCenterStreet1, treatmentCenter.treatmentCenterStreet2, treatmentCenter.treatmentCenterLat, treatmentCenter.treatmentCenterLong, treatmentCenter.treatmentCenterCity, treatmentCenter.treatmentCenterZipCode, treatmentCenter.treatmentCenterPhone, treatmentCenter.treatmentCenterWebsite FROM treatmentCenterFavorite INNER JOIN treatmentCenter ON treatmentCenter.treatmentCenterId = treatmentCenterFavorite.treatmentCenterFavoriteTreatmentCenterId WHERE treatmentCenterFavoriteProfileId = UUID_TO_BIN(:profileId)', {profileId});
 
+
+        // @ts-ignore
         return rows.length !== 0 ? {...rows[0]} : undefined;
     } catch (e) {
         console.error(e)
