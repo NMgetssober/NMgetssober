d94812828e4cc67bbfbb966a1d053435f0e6bd02
vchavez7 <vchavez7@cnm.edu>
2021-03-07 21:56:17 -0700

    created new interfaces and updated info

---
+++ backend/src/utils/interfaces/Treatmentcenter.ts
@@ -0,0 +1,12 @@
+export interface TreatmentCenter {
+    treatmentCenterId: string|null;
+    treatmentCenterName: string;
+    treatmentCenterStreet1: string;
+    treatmentCenterStreet2: string|null;
+    treatmentCenterLat: number;
+    treatmentCenterLong: number;
+    treatmentCenterCity: string;
+    treatmentCenterZipCode: string;
+    treatmentCenterPhone: string;
+    treatmentCenterWebsite: string|null;
+}
