9ebc86ce17c79fcaf8e2a95d72b910050b18cbdc
Cindy Chen <cchen4@cnm.edu>
2021-03-09 15:52:34 -0700

    finished activitybyactivitytypeidorderbyzipcode

--- backend/src/apis/activity/activity.route.ts
+++ backend/src/apis/activity/activity.route.ts
@@ -1,12 +1,19 @@
 import {Router} from "express";
-import {getActivityByProfileIdController, getActivityByActivityId} from "./activity.controller";
+import {
+    getActivityByProfileIdController,
+    getActivityByActivityId,
+    getActivityByActivityTypeIdOrderByZipCode
+} from "./activity.controller";
 import {asyncValidatorController} from "../../utils/controllers/asyncValidator.controller";
 import {check} from "express-validator";
 
 export const activityRoute = Router();
 
-activityRoute.route('/:profileId')
+activityRoute.route('/profileId/:profileId')
     .get(getActivityByProfileIdController)
 
 activityRoute.route('/:activityId')
     .get(asyncValidatorController([check("activityId", "Please provide a valid activityId").isUUID()]),getActivityByActivityId)
+
+activityRoute.route('/activityTypeId/:activityTypeId')
+    .get(asyncValidatorController([check("activityTypeId", "Please provide a valid activityId").isUUID()]),getActivityByActivityTypeIdOrderByZipCode)
