e994470093c34eab1e5d33d008adfed986cd5bf0
vchavez7 <vchavez7@cnm.edu>
2021-03-09 11:33:15 -0700

    updated treatmentcenter.controller.ts

--- backend/src/apis/treatment-center/treatmentCenter.controller.ts
+++ backend/src/apis/treatment-center/treatmentCenter.controller.ts
@@ -1,9 +1,10 @@
 import {Request, Response} from "express";
 import {Status} from "../../utils/interfaces/Status";
-import {selectTreatmentCentersByProfileId} from "../../utils/treatmentCenter/selectTreatmentCenterByProfileId";
-import {selectTreatmentCenterByTreatmentCenterId} from "../../utils/treatmentCenter/selectTreatmentCentersByTreatmentCentersId";
+import {selectTreatmentCentersByProfileId} from "../../utils/treatmentCenter/selectTreatmentCentersByProfileId";
+import {selectTreatmentCenterByTreatmentCenterId} from "../../utils/treatmentCenter/selectTreatmentCenterByTreatmentCenterId";
 
-export async function treatmentCenterController(request: Request, response: Response) : Promise<Response> {
+export async function treatmentCenterController(request: Request, response: Response): Promise<Response> {
+
     try {
         const {treatmentCentersByProfileId} = request.params;
         const mySqlResult = await selectTreatmentCentersByProfileId(treatmentCentersByProfileId);
@@ -11,26 +12,25 @@ export async function treatmentCenterController(request: Request, response: Resp
         const status: Status = {status: 200, data, message: null}
         return response.json(status)
     } catch (error) {
-        return(response.json({
+        return (response.json({
+            status: 400,
+            data: null,
+            message: error.message
+        }))
+    }
+
+
+    try {
+        const {treatmentCentersByTreatmentCentersId} = request.params;
+        const mySqlResult = await selectTreatmentCenterByTreatmentCenterId(treatmentCentersByTreatmentCentersId);
+        const data = mySqlResult ?? null
+        const status: Status = {status: 200, data, message: null}
+        return response.json(status)
+    } catch (error) {
+        return (response.json({
             status: 400,
             data: null,
             message: error.message
         }))
     }
 }
-//
-// export async function treatmentCenterController(request: Request, response: Response) : Promise<Response> {
-//     try {
-//         const {treatmentCentersByTreatmentCentersId} = request.params;
-//         const mySqlResult = await selectTreatmentCenterByTreatmentCenterId(treatmentCentersByTreatmentCentersId);
-//         const data = mySqlResult ?? null
-//         const status: Status = {status: 200, data, message: null}
-//         return response.json(status)
-//     } catch (error) {
-//         return(response.json({
-//             status: 400,
-//             data: null,
-//             message: error.message
-//         }))
-//     }
-// }
