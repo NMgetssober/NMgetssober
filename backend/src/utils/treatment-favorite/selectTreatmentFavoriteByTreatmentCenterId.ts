2443c6db21c6d8d3bf0d5a62adf5b1dc6d13fe4c
Brandon Whyte <bwhyte@cnm.edu>
2021-03-11 18:27:01 -0700

    updated selectTreatmentFavoriteByTreatmentFavoriteId

--- backend/src/utils/treatment-favorite/selectTreatmentFavoriteByTreatmentCenterId.ts
+++ backend/src/utils/treatment-favorite/selectTreatmentFavoriteByTreatmentCenterId.ts
@@ -4,7 +4,7 @@ import {TreatmentFavorite} from "../interfaces/TreatmentFavorite";
 export async function selectTreatmentFavoriteByTreatmentCenterId(treatmentFavorite: TreatmentFavorite) {
     try {
         const mysqlConnection = await connect();
-        const mySqlSelectQuery = 'SELECT BIN_TO_UUID(treatmentFavoriteTreatmentCenterId) as treatmentFavoriteTreatmentCenterId FROM treatmentFavorite WHERE treatmentFavoriteTreatmentCenterId = UUID_TO_BIN(:treatmentFavoriteTreatmentCenterId)'
+        const mySqlSelectQuery = 'SELECT BIN_TO_UUID(treatmentFavoriteTreatmentCenterId) as treatmentFavoriteTreatmentCenterId FROM treatmentFavorite WHERE treatmentFavoriteTreatmentCenterId = UUID_TO_BIN(:treatmentFavoriteTreatmentCenterId) AND treatmentFavoriteProfileId = UUID_TO_BIN(:treatmentFavoriteProfileId)'
         const [rows] = await mysqlConnection.execute(mySqlSelectQuery, treatmentFavorite)
         return rows;
 
